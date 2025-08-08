a84a762e0bd47af44e39f05dea6b6ea1e66aec5d
digiproc.dp.cr.icon4 <sigit.jatmiko@iconpln.co.id>
2025-08-08 23:06:06 +0700

    Add favicon and meta tags to fix 404 error and improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { ConfigProvider } from '../contexts/ConfigContext'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ConfigProvider>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="GitAnime - Platform streaming anime terbaik dengan koleksi terlengkap dan update terbaru" />
+        <meta name="keywords" content="anime, streaming, japan, cartoon, manga" />
+        <meta name="author" content="GitAnime" />
+        <title>GitAnime - Platform Streaming Anime Terbaik</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
