3e6e1422a513df7acd1ad6ee7c36dfc08f8ded3a
digiproc.dp.cr.icon4 <sigit.jatmiko@iconpln.co.id>
2025-08-10 02:00:19 +0700

    Perf: preconnect/dns-prefetch ke domain gambar (MAL/blogger) untuk bantu LCP

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href={process.env.NEXT_PUBLIC_BACKEND_URL || 'https://gitanime-backend.onrender.com'} />
         <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_BACKEND_URL || 'https://gitanime-backend.onrender.com'} />
+        <link rel="preconnect" href="https://cdn.myanimelist.net" />
+        <link rel="dns-prefetch" href="https://cdn.myanimelist.net" />
+        <link rel="preconnect" href="https://blogger.googleusercontent.com" />
+        <link rel="dns-prefetch" href="https://blogger.googleusercontent.com" />
       </Head>
       <body>
         <Main />
