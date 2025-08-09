39fa5d419ebba6de15a4343132782f70d0e1c82c
digiproc.dp.cr.icon4 <sigit.jatmiko@iconpln.co.id>
2025-08-10 01:03:11 +0700

    Fix: import Head di homepage; kembalikan redirects pakai named capture (:slug/:id)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { axiosGet } from '../utils/api';
 import AnimeCard from '../components/AnimeCard';
